8365c009853fb894b4ea36161dfa2a243dd7e9da
ashraful2871 <ashrafulislam2758@gmail.com>
2024-11-01 15:12:09 +0600

    mark as read book stored in local storage

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { addToStoredReadList } from "../../utility/addToDb";
 
 const BookDetail = () => {
   const { bookId } = useParams();
@@ -7,6 +8,17 @@ const BookDetail = () => {
 
   const book = data.find((book) => book.bookId === id);
   const { bookId: bId, bookName, author, category, review, tags, image } = book;
+  const handleMarkAsRead = (id) => {
+    /**
+     * 1. understand what to store to save: BookId
+     * 2. here to store: database.
+     * 3. array ,list, collection:
+     * 4. check: if book already in the readList.
+     * 5. if not, then add book to the list
+     * 6. if yes, do not add the book
+     */
+    addToStoredReadList(id);
+  };
   return (
     <div>
       <h2>book detail: {bId}</h2>
@@ -27,11 +39,14 @@ const BookDetail = () => {
                 </div>
               ))}
             </div>
-            <button className=" btn-outline btn btn-info text-white font-bold mr-5">
+            <button
+              onClick={() => handleMarkAsRead(bookId)}
+              className=" btn-outline btn btn-info text-white font-bold mr-5"
+            >
               Mark as Read
             </button>
             <button className="btn btn-info text-white font-bold">
-              Wishlist
+              Add to Wishlist
             </button>
           </div>
         </div>
